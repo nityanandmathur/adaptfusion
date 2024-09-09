ec01c90a1bf697e4af1cc685dec87edb0af6aacc
nityanandmathur <nityanandmathur@gmail.com>
2024-09-09 15:27:55 +0530

    add pre-commit, train model

---
+++ scripts/train_sdxl_lora.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
+export INSTANCE_DIR="/home/btech/nityanand.mathur/nuimages/samples"
+export OUTPUT_DIR="nuimages-samples-sdxl-lora-r4-i1000"
+export VAE_PATH="madebyollin/sdxl-vae-fp16-fix"
+
+accelerate launch --multi_gpu ../src/train_dreambooth_lora_sdxl.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --pretrained_vae_model_name_or_path=$VAE_PATH \
+  --output_dir=$OUTPUT_DIR \
+  --mixed_precision="fp16" \
+  --instance_prompt="sks scene" \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --learning_rate=1e-4 \
+  --report_to="wandb" \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=1000 \
+  --validation_prompt="a car in sks scene" \
+  --validation_epochs=100 \
+  --seed="0" \
+  --push_to_hub
